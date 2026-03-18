2f99385e0407bf46bde0569594d6d2a5b790a687
michal-owsiak <micowsiak@gmail.com>
2026-03-18 21:34:19 +0100

    names & structure refactoring

--- models/marts/whale_alert_v2.sql
+++ models/marts/whale_alert_v2.sql
@@ -4,7 +4,7 @@ with whales as (
         output_address,
         sum(output_value) as total_sent,
         count(*) as transaction_count
-    from {{ ref('stg_btc_transactions') }}
+    from {{ ref('stg_btc_non_coinbase_transactions') }}
     where output_value > 10
     group by output_address
 
