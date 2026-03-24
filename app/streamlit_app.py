cc18c0c4f5c5b6aed9fe34d19bcf4518ccec8ac9
michal-owsiak <micowsiak@gmail.com>
2026-03-24 18:37:27 +0100

    cache_data decorators added for cost & performance efficiency

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,21 +2,31 @@ import streamlit as st
 from client_snowflake import read_price_supertrend, read_halvings
 
 
-st.set_page_config(page_title="BTC Investing Tool", layout="wide")
+@st.cache_data(ttl=3600)
+def get_price_data(interval):
+    return read_price_supertrend(interval)
 
-st.title("BTC Investing Tool")
+
+@st.cache_data
+def get_halvings_data():
+    return read_halvings()
+
+
+st.set_page_config(page_title='BTC Investing Tool', layout='wide')
+
+st.title('BTC Investing Tool')
 
 interval = st.sidebar.selectbox(
-    "Interval",
-    options=["1D", "1W"],
+    'Interval',
+    options=['1D', '1W'],
     index=0
 )
 
-price_df = read_price_supertrend(interval=interval, limit=500)
-halvings_df = read_halvings()
+price_df = get_price_data(interval)
+halvings_df = get_halvings_data()
 
-st.subheader("Price data preview")
+st.subheader('Price data preview')
 st.dataframe(price_df, use_container_width=True)
 
-st.subheader("Halvings preview")
+st.subheader('Halvings preview')
 st.dataframe(halvings_df, use_container_width=True)
