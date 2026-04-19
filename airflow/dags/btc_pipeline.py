456b28f1c887814ed57f47e920de8cbf3f5912c2
michal-owsiak <micowsiak@gmail.com>
2026-04-19 20:34:32 +0200

    dbt deps command added to the 'run_dbt' task

--- airflow/dags/btc_pipeline.py
+++ airflow/dags/btc_pipeline.py
@@ -8,9 +8,6 @@ from shared.snowflake_client import get_connection
 from ingestion.load_binance_ohlc import run_ingestion
 
 
-project_root = Path(__file__).resolve().parents[1]
-
-
 @dag(
     dag_id='btc_pipeline',
     schedule='0 */4 * * *',
@@ -18,6 +15,7 @@ project_root = Path(__file__).resolve().parents[1]
     catchup=False
 )
 def btc_pipeline():
+
 
     @task(
         retries=3,
@@ -41,8 +39,8 @@ def btc_pipeline():
                 cur.execute(query)
                 print(f"Executed Snowflake task '{task_name}'")
         finally:
-            cur.close()
             conn.close()
+
 
     @task(
         retries=3,
@@ -53,8 +51,10 @@ def btc_pipeline():
         run_ingestion()
         print(f'Completed Binance OHLC ingestion')
 
+
     AIRFLOW_ROOT = Path('/opt/airflow')
     DBT_DIR = AIRFLOW_ROOT / 'dbt'
+
 
     @task(
         retries=3,
@@ -62,10 +62,22 @@ def btc_pipeline():
         retry_exponential_backoff=True,
     )
     def run_dbt():
-        print(f'DBT_DIR={DBT_DIR}')
-        print(f'DBT_DIR exists={DBT_DIR.exists()}')
-        print(f'dbt_project exists={(DBT_DIR / 'dbt_project.yml').exists()}')
-        print(f'profiles exists={(DBT_DIR / 'profiles.yml').exists()}')
+        deps = subprocess.run(
+            [
+                '/home/airflow/.local/bin/dbt',
+                'deps',
+                '--project-dir', str(DBT_DIR),
+                '--profiles-dir', str(DBT_DIR),
+            ],
+            cwd=str(DBT_DIR),
+            capture_output=True,
+            text=True,
+        )
+        
+        if deps.returncode != 0:
+            raise Exception(
+                f'DBT DEPS FAILED:\nreturn code={deps.returncode}\nstdout={deps.stdout}\nstderr={deps.stderr}'
+            )
 
         result = subprocess.run(
             [
@@ -79,18 +91,13 @@ def btc_pipeline():
             text=True,
         )
 
-        print('DBT RETURN CODE')
-        print(result.returncode)
-        print('DBT STDOUT')
-        print(result.stdout)
-        print('DBT STDERR')
-        print(result.stderr)
-
         if result.returncode != 0:
             raise Exception(
                 f'DBT FAILED:\nreturn code={result.returncode}\nstdout={result.stdout}\nstderr={result.stderr}'
         )
 
+
     run_snowflake_task() >> run_binance_ingestion() >> run_dbt()
+
 
 btc_pipeline()
